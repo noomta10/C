52b8eafcb8236fb0d71a6ef8723cf3c33f972fbc
noomt <noomta10@gmail.com>
2023-03-09 09:20:15 +0200

    b

--- Main.c
+++ Main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void main0();
 void main1();
 void main2();
 void main3();
@@ -9,11 +10,13 @@ void main6();
 void main7();
 void main8();
 void main9();
+void main10();
+
 void demo1();
 void demo2();
 void demo3();
 void demo4();
 
 main() {
-	main9();
+	main10();
 }
