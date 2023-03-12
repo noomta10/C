a9f453c7535addb26e34964d014126ea05ea4331
noomt <noomta10@gmail.com>
2023-03-12 08:42:09 +0200

    baseeee

---
+++ 4-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int strindex(char s[], char t[]);
+
+main11() {
+	char s[] = "bye hello";
+	char t[] = "hello";
+
+	printf("Right most occurance of: %s\nIn string: %s\nIs: %d\n", t, s, strindex(s,t));
+	
+}
+
+int strindex(char s[], char t[]) {
+	int s_index, t_index, temp_index;
+
+	for (s_index = 0; s[s_index] != '\0'; s_index++) {
+		for (t_index = s_index, temp_index = 0; t[temp_index] != '\0' && s[t_index] == t[temp_index]; t_index++, temp_index++)
+			;
+		if (temp_index > 0 && t[temp_index] == '\0')
+			return s_index;
+		return -1;
+
+	}
+}
