39d03ee55aa5867cf9ebce6cd37936add1538396
noam <noomta10@gmail.com>
2023-03-22 17:51:12 +0200

    HHH

--- Mmn11-1/my_string.c
+++ Mmn11-1/my_string.c
@@ -8,8 +8,13 @@ Returns <0 if cs<ct, 0 if cs=ct, or 0< if cs>ct.
 Returns int of first occurance of c in cs, or -1 if not present. */
 
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #define MAX_LINE 80
+#define OPTION1 1
+#define OPTION2 2
+#define OPTION3 3
+#define OPTION4 4 
 #define SMALL -1
 #define BIG 1
 #define EQUAL 0
@@ -22,48 +27,53 @@ int my_strncmp(char cs[], char ct[], int n);
 int my_strchr(char cs[], char c);
 
 
-/* Main function prints a menu and lets the user choose between 3 functions.
+/* Main function prints a menu and lets the user choose between 3 functions. The process repeats until the user types 4 to exit.
 The function is selected according to the number the user typed.
 Required values are prompted, and sent to the function.
 In the end, the function prints the return value of the selected function. */
 
 int main() {
-	int option;
-	printf("\nChoose a function to use:\n\n"
-		"1. my_strcmp(cs, ct)- Compare string cs to ct. return <0 if cs<ct, 0 if cs=ct, or 0< if cs>ct.\n\n"
-		"2. my_strncmp(cs, ct, n)- Compare at most n characters of string cs to ct. return <0 if cs<ct, 0 if cs=ct, or 0< if cs>ct.\n\n"
-		"3. my_strchr(cs, c)- Return int of first occurance of c in cs, or -1 if not present.\n\n"
-		"Enter your choise: ");
-	scanf("%d", &option);
-
-	if (option == 1) {
-		char first_string[MAX_LINE];
-		char second_string[MAX_LINE];
-		printf("Enter 2 strings with a space between them: ");
-		scanf("%s%s", first_string, second_string);
-		printf("Return value: %d\n", my_strcmp(first_string, second_string));
+	int option = OK;
+	while (option != OPTION4)
+	{
+		printf("\nChoose a function to use:\n\n"
+			"1. my_strcmp(cs, ct)- Compare string cs to ct. return <0 if cs<ct, 0 if cs=ct, or 0< if cs>ct.\n\n"
+			"2. my_strncmp(cs, ct, n)- Compare at most n characters of string cs to ct. return <0 if cs<ct, 0 if cs=ct, or 0< if cs>ct.\n\n"
+			"3. my_strchr(cs, c)- Return int of first occurance of c in cs, or -1 if not present.\n\n"
+			"4. quit\n\n"
+			"Enter your choise: ");
+		scanf("%d", &option);
+		if (option == OPTION1) {
+			char first_string[MAX_LINE];
+			char second_string[MAX_LINE];
+			printf("Enter 2 strings with a space between them: ");
+			scanf("%s%s", first_string, second_string);
+			printf("Return value: %d\n\n", my_strcmp(first_string, second_string));
+		}
+		else if (option == OPTION2) {
+			char first_string[MAX_LINE];
+			char second_string[MAX_LINE];
+			int characters_to_compare;
+			printf("Enter 2 strings with a space between them: ");
+			scanf("%s%s", first_string, second_string);
+			printf("Enter number of characters to compare: ");
+			scanf("%d", &characters_to_compare);
+			printf("Return value: %d\n\n", my_strncmp(first_string, second_string, characters_to_compare));
+		}
+		else if (option == OPTION3) {
+			char string[MAX_LINE];
+			char character;
+			printf("Enter a string: ");
+			scanf("%s", string);
+			printf("Enter a character: ");
+			scanf(" %c", &character);
+			printf("Return value: %d\n\n", my_strchr(string, character));
+		}
+		else if (option == OPTION4)
+			break;
+		else
+			printf("Invalid option.\n\n");
 	}
-	else if (option == 2) {
-		char first_string[MAX_LINE];
-		char second_string[MAX_LINE];
-		int characters_to_compare;
-		printf("Enter 2 strings with a space between them: ");
-		scanf("%s%s", first_string, second_string);
-		printf("Enter number of characters to compare: ");
-		scanf("%d", &characters_to_compare);
-		printf("Return value: %d\n", my_strncmp(first_string, second_string, characters_to_compare));
-	}
-	else if (option == 3) {
-		char string[MAX_LINE];
-		char character;
-		printf("Enter a string: ");
-		scanf("%s", string);
-		printf("Enter a character: ");
-		scanf(" %c", &character);
-		printf("Return value: %d\n", my_strchr(string, character));
-	}
-	else
-		printf("Invalid option.\n");
 
 	return OK;
 }
