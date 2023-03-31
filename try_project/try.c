e9889fb330fa1e42346f0e10826731e8e02c1424
noam <noomta10@gmail.com>
2023-03-31 22:33:31 +0300

    h

--- try_project/try.c
+++ try_project/try.c
@@ -1,14 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
+﻿#include <stdio.h> 
 
-int main() {
-	char source[10] = "123";
-	char destination[10] = "abcde";
 
-	printf("destination is originally = '%s'\n", destination);
-	strncpy(destination, source, 5);
-	printf("After strncpy, destination becomes '%s'\n", destination);
+main()
+{
+	// DOES NOT WORK
+	char* y = "1234";
+	char x[] = y;
 
-	return(0);
+	// WORKS
+	char y[] = "1234";
+	char* x = y;
 }
