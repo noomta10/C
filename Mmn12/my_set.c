fa2aeeab6507f55936c02efa5ce5d6ced2a21229
noam <noomta10@gmail.com>
2023-04-02 21:10:31 +0300

    h

--- Mmn12/my_set.c
+++ Mmn12/my_set.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <stdlib.h>
-#define ENLARGE_SIZE sizeof(int);
+#define ENLARGE_SIZE sizeof(int)
 
 
 int* get_set(void);
@@ -17,10 +17,10 @@ int main() {
 int* get_set(void) {
 	int number; // users number
 	int count = 0; // count all numbers entered by the user
-	int size = 1; // Initial value of array
-	int* array = malloc(size * sizeof(int)); // allocate memory for the array
+	int size = 1; // initial value of array
+	int* array_pointer = malloc(size * ENLARGE_SIZE); // allocate memory for the array
 
-	if (array == NULL) {
+	if (array_pointer == NULL) {
 		printf("Error: memory allocation failed\n");
 		exit(1);
 	}
@@ -29,25 +29,24 @@ int* get_set(void) {
 	while (scanf("%d", &number) == 1) {
 		if (count == size) {
 			size += ENLARGE_SIZE;
-			array = realloc(array, size);
+			array_pointer = realloc(array_pointer, size);
 
-			if (array == NULL) {
+			if (array_pointer == NULL) {
 				printf("Error: memory reallocation failed\n");
 				exit(1);
 			}
 		}
 		
-		array[count++] = number;
+		array_pointer[count++] = number;
 	}
-
-	return array;
+	array_pointer[count++] = '\0';
+	return array_pointer;
 }
 
 
 
 void print_set(int *array) {
-	int count = 5;
-	for (int i = 0; i < count; i++) {
+	for (int i = 0; array[i] != '\0' ; i++) {
 		printf("%d ", array[i]);
 	}
 }
