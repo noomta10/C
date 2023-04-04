5e53186ae86643f7b25e6413fe7a9294120df7f8
noam <noomta10@gmail.com>
2023-04-04 21:27:47 +0300

    tomorrow ill finish it

--- Mmn12/my_set.c
+++ Mmn12/my_set.c
@@ -14,11 +14,10 @@
 #define FIRST_ITERATION 1
 #define NOT_FIRST_ITERATION 0
 
-
-
 int* get_set(void);
 void print_set(int* set_pointer, int number_of_elements);
 int number_in_array(int number, int* array_pointer, int number_of_elements);
+void add_number_to_array(int** pointer_to_pointer, int number, int size, int index);
 
 int main(void) {
 	int* set_pointer = get_set();
@@ -31,39 +30,39 @@ int main(void) {
 // create a set
 int* get_set(void) {
 	int number; // users number
-	int index = FIRST_INDEX; // index of number in the array
-	int size = INITIAL_ARRAY_SIZE; // initial size of array
-	int* array_pointer = (int*)malloc(size); // allocate memory for the array
-	int first_iteration = FIRST_ITERATION; // first iteration in while loop to print a title
+	int set_index = FIRST_INDEX; // index of number in the set
+	int set_size = INITIAL_ARRAY_SIZE; // initial size of set
+	int* set_pointer = (int*)malloc(set_size); // allocate memory for the set pointer
+	int full_array_index = FIRST_INDEX; // index of number in the full array
+	int full_array_size = INITIAL_ARRAY_SIZE; // initial size of full array
+	int* full_array_pointer = (int*)malloc(full_array_size); // allocate memory for the full array pointer 
 
-	if (array_pointer == NULL) {
+	if (set_pointer == NULL || full_array_pointer == NULL) {
 		printf("Error: memory allocation failed\n");
 		exit(MEMORY_ERROR);
 	}
 
 	printf("Enter numbers:\n");
 	while (scanf("%d", &number) == INT_FOUND) {
-		if (first_iteration) {
-			printf("Numbers are:\n");
-			first_iteration = NOT_FIRST_ITERATION;
-		}
-		printf("%d ", number);
+		full_array_size++;
+		add_number_to_array(&set_pointer, number, set_size, set_index);
+		full_array_index++;
 
-		if (!number_in_array(number, array_pointer, size)) {
-			size++;
-			array_pointer = realloc(array_pointer, size * ENLARGE_SIZE);
-
-			if (array_pointer == NULL) {
-				printf("Error: memory reallocation failed\n");
-				exit(MEMORY_ERROR);
-			}
-
-			*(array_pointer + index++) = number;
+		if (!number_in_array(number, set_pointer, set_size)) {
+			set_size++;
+			add_number_to_array(&set_pointer, number, set_size, set_index);
+			set_index++;
 		}
 	}
 
-	array_pointer[NUMBER_OF_ELEMENTS_INDEX] = --index;
-	return array_pointer;
+	full_array_pointer[NUMBER_OF_ELEMENTS_INDEX] = --full_array_index;
+	printf("Numbers are:\n");
+	for (int index = 1; index <= full_array_pointer[NUMBER_OF_ELEMENTS_INDEX]; index++) {
+		printf("%d ", *(full_array_pointer + index));
+	}
+
+	set_pointer[NUMBER_OF_ELEMENTS_INDEX] = --set_index;
+	return set_pointer;
 }
 
 // print a set
@@ -84,4 +83,15 @@ int number_in_array(int number, int* array_pointer, int number_of_elements)
 	}
 
 	return NUMBER_DOESNT_EXIST;
+}
+
+void add_number_to_array(int** pointer_to_pointer, int number, int size, int index) {
+	*pointer_to_pointer = realloc(*pointer_to_pointer, size * ENLARGE_SIZE);
+
+	if (*pointer_to_pointer == NULL) {
+		printf("Error: memory reallocation failed\n");
+		exit(MEMORY_ERROR);
+	}
+
+	*(*pointer_to_pointer + index++) = number;
 }
