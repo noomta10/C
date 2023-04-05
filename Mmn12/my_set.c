0c9115c2dee475b23cab38efc3d551b07af16c80
noam <noomta10@gmail.com>
2023-04-05 10:34:53 +0300

    worksssss

--- Mmn12/my_set.c
+++ Mmn12/my_set.c
@@ -8,16 +8,16 @@
 #define NUMBER_OF_ELEMENTS_INDEX 0
 #define NO_ERRORS 0
 #define MEMORY_ERROR 1
-#define INT_FOUND 1 
+#define INT_FOUND 1
 #define INITIAL_ARRAY_SIZE 1
 #define FIRST_INDEX 1
-#define FIRST_ITERATION 1
-#define NOT_FIRST_ITERATION 0
 
 int* get_set(void);
 void print_set(int* set_pointer, int number_of_elements);
-int number_in_array(int number, int* array_pointer, int number_of_elements);
 void add_number_to_array(int** pointer_to_pointer, int number, int size, int index);
+int number_in_array(int number, int* array_pointer, int number_of_elements);
+void print_input(int* full_array_pointer, int number_of_elements);
+
 
 int main(void) {
 	int* set_pointer = get_set();
@@ -27,15 +27,16 @@ int main(void) {
 	return NO_ERRORS;
 }
 
-// create a set
+
+// Create a set
 int* get_set(void) {
-	int number; // users number
-	int set_index = FIRST_INDEX; // index of number in the set
-	int set_size = INITIAL_ARRAY_SIZE; // initial size of set
-	int* set_pointer = (int*)malloc(set_size); // allocate memory for the set pointer
-	int full_array_index = FIRST_INDEX; // index of number in the full array
-	int full_array_size = INITIAL_ARRAY_SIZE; // initial size of full array
-	int* full_array_pointer = (int*)malloc(full_array_size); // allocate memory for the full array pointer 
+	int number; // Users number
+	int set_index = FIRST_INDEX; // Index of number in set
+	int set_size = INITIAL_ARRAY_SIZE; // Initial size of set
+	int* set_pointer = (int*)malloc(set_size); // Allocate memory for the set pointer
+	int full_array_index = FIRST_INDEX; // Index of number in full array
+	int full_array_size = INITIAL_ARRAY_SIZE; // Initial size of full array
+	int* full_array_pointer = (int*)malloc(full_array_size); // Allocate memory for the full array pointer 
 
 	if (set_pointer == NULL || full_array_pointer == NULL) {
 		printf("Error: memory allocation failed\n");
@@ -45,7 +46,7 @@ int* get_set(void) {
 	printf("Enter numbers:\n");
 	while (scanf("%d", &number) == INT_FOUND) {
 		full_array_size++;
-		add_number_to_array(&set_pointer, number, set_size, set_index);
+		add_number_to_array(&full_array_pointer, number, full_array_size, full_array_index);
 		full_array_index++;
 
 		if (!number_in_array(number, set_pointer, set_size)) {
@@ -55,36 +56,28 @@ int* get_set(void) {
 		}
 	}
 
-	full_array_pointer[NUMBER_OF_ELEMENTS_INDEX] = --full_array_index;
-	printf("Numbers are:\n");
-	for (int index = 1; index <= full_array_pointer[NUMBER_OF_ELEMENTS_INDEX]; index++) {
-		printf("%d ", *(full_array_pointer + index));
-	}
+	int full_array_number_of_elements = --full_array_index;
+	full_array_pointer[NUMBER_OF_ELEMENTS_INDEX] = full_array_number_of_elements;
+	print_input(full_array_pointer, full_array_number_of_elements);
 
 	set_pointer[NUMBER_OF_ELEMENTS_INDEX] = --set_index;
 	return set_pointer;
 }
 
-// print a set
+
+// Print a set
 void print_set(int* set_pointer, int number_of_elements) {
-	printf("\nSet is:\n");
-	for (int index = 1; index <= number_of_elements; index++) {
+	int index = FIRST_INDEX;
+	printf("Set is:\n");
+
+	for (index; index <= number_of_elements; index++)
 		printf("%d ", *(set_pointer + index));
-	}
+
 	printf("\n");
 }
 
-// check if number is present in the array
-int number_in_array(int number, int* array_pointer, int number_of_elements)
-{
-	for (int i = 0; i < number_of_elements; i++) {
-		if (array_pointer[i] == number)
-			return NUMBER_EXISTS;
-	}
 
-	return NUMBER_DOESNT_EXIST;
-}
-
+// Add number to the array
 void add_number_to_array(int** pointer_to_pointer, int number, int size, int index) {
 	*pointer_to_pointer = realloc(*pointer_to_pointer, size * ENLARGE_SIZE);
 
@@ -94,4 +87,30 @@ void add_number_to_array(int** pointer_to_pointer, int number, int size, int ind
 	}
 
 	*(*pointer_to_pointer + index++) = number;
+}
+
+
+// Check if number is present in the array
+int number_in_array(int number, int* array_pointer, int number_of_elements)
+{
+	int index = FIRST_INDEX;
+
+	for (index; index < number_of_elements; index++) {
+		if (array_pointer[index] == number)
+			return NUMBER_EXISTS;
+	}
+
+	return NUMBER_DOESNT_EXIST;
+}
+
+
+// Print all the numbers the user entered 
+void print_input(int* full_array_pointer, int number_of_elements) {
+	int index = FIRST_INDEX;
+	printf("Numbers received are:\n");
+
+	for (index; index <= number_of_elements; index++)
+		printf("%d ", *(full_array_pointer + index));
+
+	printf("\n");
 }
