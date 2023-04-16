20c368a25dad90594c0d94a6475b8b2a25d98366
noam <noomta10@gmail.com>
2023-04-16 21:41:39 +0300

    h

--- Mmn22/mycomp.c
+++ Mmn22/mycomp.c
@@ -3,29 +3,42 @@
 #include <stdio.h>
 #include <string.h>
 #include "complex.h"
+typedef enum {FALSE, TRUE};
 
-complex A = { 0, 0 };
-complex B = { 0, 0 };
-complex C = { 0, 0 };
-complex D = { 0, 0 };
-complex E = { 0, 0 };
-complex F = { 0, 0 };
 
-char* check_command(char command[]) {
-	char* command_name = strtok(command, " ");
-	return command_name;
+char* check_command(char line[]) {
+	int i;
+	int valid_command = FALSE;
+	char* valid_commands[] = {"read_comp", "print_comp", "add_comp", "sub_comp", "mult_comp_real", "mult_comp_img", "mult_comp_comp", "abs_comp"};
+	int number_of_commands = sizeof(valid_commands) / sizeof(char*);
+    char* command = strtok(line, " ");
+
+	for (i = 0; i < number_of_commands; i++) {
+		if (strcmp(command, valid_commands[i]) == 0) {
+			valid_command = TRUE;
+			return command;
+		}
+	}
+
+	return "Undefined command name";
 }
 
 int main() {
-	char command[100];
+	complex A = { 0, 0 };
+	complex B = { 0, 0 };
+	complex C = { 0, 0 };
+	complex D = { 0, 0 };
+	complex E = { 0, 0 };
+	complex F = { 0, 0 };
+	char line[100];
 	printf("Enter a command:\n");
-	scanf(" %[^\n]", command);
+	scanf(" %[^\n]", line);
 
-	while (strcmp(command, "stop") != 0) {
-		printf("%s\n", command);
-		printf("%s\n", check_command(command));
+	while (strcmp(line, "stop") != 0) {
+		printf("%s\n", line);
+		printf("%s\n", check_command(line));
 		printf("Enter a command:\n");
-		scanf("%[^\n]", command);
+		scanf(" %[^\n]", line);
 	}
 }
 
