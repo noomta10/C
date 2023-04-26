e1df0372e693959c5985154857f5d519008c7a12
noam <noomta10@gmail.com>
2023-04-26 18:50:00 +0300

    home mmn22

--- Mmn22/complex.c
+++ Mmn22/complex.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "complex.h"
 
 void read_comp(complex *variable, double real, double imaginary) {
@@ -27,14 +28,28 @@ complex sub_comp(complex first_variable, complex second_variable) {
 }
 
 complex mult_comp_real(complex variable, double real_number) {
-	complex product;
-	product.real = variable.real * real_number;
-	product.imaginary = variable.imaginary * real_number;
-	return product;
+	complex real_product;
+	real_product.real = variable.real * real_number;
+	real_product.imaginary = variable.imaginary * real_number;
+	return real_product;
 }
 
 complex mult_comp_img(complex variable, double imaginary_number) {
-	complex product;
+	complex imaginary_product;
+	imaginary_product.real = -imaginary_number * variable.imaginary;
+	imaginary_product.imaginary = imaginary_number * variable.real;
+	return imaginary_product;
+}
 
-	return product;
+complex mult_comp_comp(complex first_variable, complex second_variable) {
+	complex complex_product;
+	complex_product.real = (first_variable.real * second_variable.real) - (first_variable.imaginary * second_variable.imaginary);
+	complex_product.imaginary = (first_variable.real * second_variable.imaginary) + (first_variable.imaginary * second_variable.real);
+	return complex_product;
+}
+
+double abs_comp(complex variable) {
+	double abs_result;
+	abs_result = sqrt(pow(variable.real, 2) + pow(variable.imaginary, 2));
+	return abs_result;
 }
