eae83ce897fee80728022d4e84d68bce5d6eecd3
noam <noomta10@gmail.com>
2023-04-28 19:53:16 +0300

    h

--- Mmn22/mycomp.c
+++ Mmn22/mycomp.c
@@ -59,18 +59,26 @@ int main() {
 	complex D = { 0, 0 };
 	complex E = { 0, 0 };
 	complex F = { 0, 0 };
-
-	char line[100];
-
+	int c;
+	int line_size = 0;
+	char* line = (char*)malloc(++line_size * CHAR_SIZE);
 
 	printf("Enter a command:\n");
-	scanf(" %[^\n]", line);
+	c = getchar();
+	while (c != '\n') {
+		line[line_size - 1] = c;
+		line = realloc(line, ++line_size * CHAR_SIZE);
+		if (line == NULL) {
+			printf("Error: memory allocation failed\n");
+			exit(-1);
+		}
+		c = getchar();
+	}
+	line[line_size - 1] = '\0';
 
 	while (strcmp(line, "stop") != 0) {
 		printf("Line received is: %s\n", line);
-		
 		check_line(line);
-		printf("LINE IN MAIN: %s", line);
 		printf("Enter a command:\n");
 		scanf(" %[^\n]", line);
 	}
--- try_project/try.c
+++ try_project/try.c
@@ -1,5 +1,7 @@
 ﻿#include <stdio.h> 
 
 int main() {
+	char* my_string = "HEY";
+	printf("SIZE: %d\n", sizeof(my_string) / sizeof(char));
 	return 0;
 }
