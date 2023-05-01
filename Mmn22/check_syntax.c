632c2a1ec6ac974c303fab4636b40942835882e4
noam <noomta10@gmail.com>
2023-05-01 21:07:11 +0300

    noam was here

--- Mmn22/check_syntax.c
+++ Mmn22/check_syntax.c
@@ -1,49 +1,55 @@
 #include "complex.h"
 
-int variable_valid(char variable) {
+char check_variable(char variable_string[]) {
+	char variable_character;
 	int index;
 	int valid_variable = 0;
 	char valid_variables[6] = { 'A', 'B', 'C', 'D', 'E', 'F' };
 
-	for (index = 0; index < 6; index++) {
-		if (variable == valid_variables[index])
-			valid_variable = 1;
-	}
-
-	return valid_variable;
-}
-
-void check_read_comp_syntax(command_line* user_command_line, complex* complex_array[]) {
-	char* variable_string = get_string(user_command_line, ',');
-	char variable_character = variable_string[0];
-	double real_input;
-	double imaginary_input;
-
 	if (strlen(variable_string) < 1)
 		printf("Missing parameter\n");
 
-	if (!variable_valid(variable_character))
+	variable_character = variable_string[0];
+;
+	for (index = 0; index < 6; index++) {
+		if (variable_character == valid_variables[index])
+			valid_variable = 1;
+	}
+
+	if (!valid_variable) {
 		printf("Undefined complex variable\n");
+	}
+
+	return variable_character;
+}
+
+void check_read_comp_syntax(command_line* user_command_line, complex* complex_array[]) {
+	double real_input;
+	double imaginary_input;
+	char* variable_string = get_string(user_command_line, ',');
+	char variable = check_variable(variable_string);
 
 	real_input = atof(get_string(user_command_line, ','));
 	imaginary_input = atof(get_string(user_command_line, '\n'));
 
 	printf("%lf\n", real_input);
 	printf("%lf\n", imaginary_input);
-	read_comp(complex_array['A' - variable_character], real_input, imaginary_input);
+	read_comp(complex_array['A' - variable], real_input, imaginary_input);
 }
 
 
 void check_print_comp_syntax(command_line* user_command_line, complex* complex_array[]) {
 	char* variable_string = get_string(user_command_line, '\n');
-	char variable_character = variable_string[0];
-	
-	if (strlen(variable_string) < 1)
-		printf("Missing parameter\n");
+	char variable = check_variable(variable_string);
 
-	if (!variable_valid(variable_character))
-		printf("Undefined complex variable\n");
+	print_comp(*complex_array['A' - variable]);
+}
 
-	print_comp(complex_array['A' - variable_character]);
+void check_add_comp_syntax(command_line* user_command_line, complex* complex_array[]) {
+	char* variable_string1 = get_string(user_command_line, ',');
+	char variable1 = check_variable(variable_string1);
+	char* variable_string2 = get_string(user_command_line, '\n');
+	char variable2 = check_variable(variable_string2);
 
+	complex sum_result = add_comp(*complex_array['A' - variable1], *complex_array['A' - variable2]);
 }
--- Mmn22/complex.h
+++ Mmn22/complex.h
@@ -17,10 +17,28 @@ typedef struct {
 	int parse_index;
 } command_line;
 
+
+// Main
 char* get_line(char* line, int line_size);
 char* get_string(command_line* command_line, char seperator);
 void check_command(char* command, command_line* user_command_line, complex* complex_array[]);
 
+
+// Check_syntax
+char check_variable(char variable_string[]);
 void check_read_comp_syntax(command_line* user_command_line, complex* complex_array[]);
 void check_print_comp_syntax(command_line* user_command_line, complex* complex_array[]);
-int variable_valid(char variable);
+void check_add_comp_syntax(command_line* user_command_line, complex* complex_array[]);
+
+
+// Complex
+void read_comp(complex* variable, double real, double imaginary);
+void print_comp(complex variable);
+complex add_comp(complex first_variable, complex second_variable);
+complex sub_comp(complex first_variable, complex second_variable);
+complex mult_comp_real(complex variable, double real_number);
+complex mult_comp_img(complex variable, double imaginary_number);
+complex mult_comp_comp(complex first_variable, complex second_variable);
+double abs_comp(complex variable);
+
+
