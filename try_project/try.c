bf73ab6b66a1e7031299aa3dc47db0d845a2edce
noam <noomta10@gmail.com>
2023-05-12 19:06:09 +0300

    h

--- try_project/try.c
+++ try_project/try.c
@@ -1,68 +1,56 @@
-<<<<<<< HEAD
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #define NUM 15 
-
 /* point record description */
-struct point{
+struct point
+{
 	int x, y, z;
 };
-
-/* writes and then reads NUM records from the file "points_file". */
-int main() {
+/* writes and then reads NUM records
+ from the file "points_file". */
+int main()
+{
 	int i, j;
 	FILE* f;
+	int r1, r2;
 	struct point pnt;
-
 	/* create a file of NUM records */
 	f = fopen("points_file", "w");
 	if (!f)
 		return 1;
-	for (i = 1;i <= NUM; i++) {
+
+	pnt.y = 0;
+	pnt.z = 0;
+	for (i = 1;i <= NUM; i++)
+	{
 		pnt.x = i;
-		fwrite(&pnt, sizeof(struct point), 1, f);
+		pnt.y = i;
+		fwrite(&pnt, 12, 1, f);
 	}
 	fclose(f);
-
 	/* read the NUM records */
 	f = fopen("points_file", "r");
 	if (!f)
 		return 1;
-	for (i = 1;i <= NUM; i++) {
+	for (i = 1;i <= NUM; i++)
+	{
 		fread(&pnt, sizeof(struct point), 1, f);
 		printf("%d\n", pnt.x);
 	}
 	fclose(f);
 	printf("\n");
-
-	/* use fseek to read the NUM records in reverse order */
+	/* use fseek to read the NUM records
+	in reverse order */
 	f = fopen("points_file", "r");
 	if (!f)
 		return 1;
-	for (i = NUM - 1; i >= 0; i--) {
-		fseek(f, sizeof(struct point) * i, SEEK_SET);
-		fread(&pnt, sizeof(struct point), 1, f);
-		printf("%d\n", pnt.x);
+	for (i = NUM - 1; i >= 0; i--)
+	{
+		r1= fseek(f, sizeof(struct point) * i, SEEK_SET);
+		r2 = fread(&pnt, sizeof(struct point), 1, f);
+		printf("%d %d %d\n", r1, r2, pnt.x);
 	}
 	fclose(f);
 	printf("\n");
 	return 0;
 }
-=======
-﻿#define _CRT_SECURE_NO_WARNINGS 
-
-#include <stdio.h>
-
-
-main()
-{
-    FILE* fp;
-fp = fopen("Hello there.bye", "r");
-/* There can't be a file with such a name */
-if (fp == NULL)
-{
-    perror("myprog");
-    exit(7);
-}
-}
->>>>>>> f56d696bdd001869b4503ebbc4d87266f8e4e8cd
