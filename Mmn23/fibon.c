e05ad770e36dcc2d364704760c165c0befff7e77
noomt <noomta10@gmail.com>
2023-05-18 08:10:16 +0300

    bbbbbbbbb

--- Mmn23/fibon.c
+++ Mmn23/fibon.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct Node {
     int number;
     struct Node* next;
 } Node;
 
 
-void write_fibonacci_file(Node* first_node) {
-    FILE* file_pointer = fopen("fibonacci.txt", "a");
+void write_fibonacci_file(Node* first_node, int number_of_elements, char* file_name) {
+    FILE* file_pointer = fopen(file_name, "w");
     Node* current_node = first_node;
 
     if (file_pointer == NULL) {
@@ -17,11 +18,14 @@ void write_fibonacci_file(Node* first_node) {
         exit(-2);
     }
 
+    fprintf(file_pointer, "First %d elements in fibonacci sequence when n=%d", number_of_elements, number_of_elements);
+    fclose(file_pointer);
+    file_pointer = fopen(file_name, "a");
+
     do {
         fprintf(file_pointer, "%d ", current_node->number);
         current_node = current_node->next;
     } while (current_node != first_node);
-
 
     fclose(file_pointer);
 }
@@ -31,7 +35,7 @@ void print_fibonacci(Node* first_node, int number_of_elements) {
     struct Node* previous_node = NULL;
     struct Node* current_node = first_node;
     struct Node* next_node;
-    printf("Descending order of first %d elements in fibonacci sequence :\n", number_of_elements);
+    printf("Descending order of first %d elements in fibonacci sequence:\n", number_of_elements);
 
     /* Reverse to descending order */
     do {
@@ -70,6 +74,9 @@ Node* create_fibonacci(int number_of_elements) {
     Node* second_node = create_node(1);
     first_node->next = second_node;
 
+    if (number_of_elements == 0)
+        return(first_node);
+
     Node* current_node = second_node;
     Node* previous_node = first_node;
 
@@ -84,14 +91,42 @@ Node* create_fibonacci(int number_of_elements) {
     return first_node;
 }
 
+void check_arguments_number(argc) {
+    if (argc > 2) {
+        fprintf(stderr, "Error: too many arguments\n");
+        exit(-1);
+    }
+    if (argc < 2) {
+        fprintf(stderr, "Error: too few arguments\n");
+        exit(-1);
+    }
+}
 
-int main() {
-    int number_of_elements;
+
+int check_elements_number(int number_of_elements) {
     printf("Enter number of elements: ");
     scanf("%d", &number_of_elements);
 
+    while (number_of_elements < 0) {
+        fprintf(stderr, "Error: number of elements need to be a positive number\n");
+        printf("Enter number of elements: ");
+        scanf("%d", &number_of_elements);
+    }
+
+    return number_of_elements;
+}
+
+
+int main(int argc, char* argv[]) {
+    int number_of_elements = NULL;
+    
+    check_arguments_number(argc);
+    char* file_name = argv[1];
+
+    number_of_elements = check_elements_number(number_of_elements);
+
     Node* first_node = create_fibonacci(number_of_elements);
-    write_fibonacci_file(first_node);
+    write_fibonacci_file(first_node, number_of_elements, file_name);
     print_fibonacci(first_node, number_of_elements);
     return 0;
 }
