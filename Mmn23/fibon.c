e993e73c0393126b4fc16d9053a0738725c407da
noam <noomta10@gmail.com>
2023-05-20 17:29:23 +0300

    fix names

--- Mmn23/fibon.c
+++ Mmn23/fibon.c
@@ -18,7 +18,7 @@ void free_memory(Node** first_node_pointer) {
 It creates a file, prints an error if one was encountered, and prints a title.
 Then it loops through the sequence and prints the sequence in a descending order. 
 Finally, it closes the file. Does not return anything. */
-void write_fibonacci_file(Node* first_node, int number_of_elements, char* file_name) {
+void write_fibonacci_file(Node* first_node, int n, char* file_name) {
     FILE* file_pointer = fopen(file_name, "w");
     Node* current_node = first_node;
 
@@ -27,7 +27,7 @@ void write_fibonacci_file(Node* first_node, int number_of_elements, char* file_n
         exit(MEMORY_ERROE);
     }
 
-    fprintf(file_pointer, "First elements in fibonacci sequence when maximun index n = %d\n", number_of_elements, number_of_elements);
+    fprintf(file_pointer, "First elements in fibonacci sequence when maximun index n = %d\n", n);
     fclose(file_pointer);
     file_pointer = fopen(file_name, "a");
 
@@ -42,11 +42,10 @@ void write_fibonacci_file(Node* first_node, int number_of_elements, char* file_n
 
 /* Print_fibonnaci function gets a Node pointer to the first node and the number of elements in the sequence.
 It prints the sequence in a descending order. Does not return anything. */
-void print_fibonacci(Node* first_node, int number_of_elements) {
+void print_fibonacci(Node* first_node, int n) {
     struct Node* current_node = first_node;
-    printf("First elements in fibonacci sequence when maximun index n = %d\n", number_of_elements);
+    printf("First elements in fibonacci sequence when maximun index n = %d\n", n);
     
-    /* Print the sequence in descending order */
     do {
         printf("%d ", (current_node->previous)->number);
         current_node = current_node->previous;
@@ -106,12 +105,12 @@ Node* create_fibonacci(int n) {
 }
 
 
-/* Check_arguments function gets the number of arguments- int argc,
+/* Get_file_name function gets the number of arguments- int argc,
 and a pointer array which points to each argument passed to the program in the command line- char* argv[].
 The function checks that the number of arguments that were passed in the command line is valid,
 if it is not, it print an error message and exit the program.
 If no errors were encounterd, returns the file name which is a pointer to characters */
-char* check_arguments(int argc, char* argv[]) {
+char* get_file_name(int argc, char* argv[]) {
     char* file_name;
     if (argc > 2) {
         fprintf(stderr, "Error: too many arguments\n");
@@ -127,20 +126,20 @@ char* check_arguments(int argc, char* argv[]) {
 }
 
 
-/* Check_elements_number gets the number of elements the user entered- int number_of_elements.
+/* Get_n_input function gets the number of elements the user entered- int n.
 The function checks that the number is positive, if it is not, it prints an error message ands asks again for an input.
-Returns a valid number of elements- int number_of_elements */
-int check_elements_number(int number_of_elements) {
+Returns a valid number of n (maximum index in fibonacci sequence)- int n */
+int get_n_input(int n) {
     printf("Enter n (maximum index in the sequence): ");
-    scanf("%d", &number_of_elements);
+    scanf("%d", &n);
 
-    while (number_of_elements < 0) {
+    while (n < 0) {
         fprintf(stderr, "Error: number of elements need to be a positive number\n");
         printf("Enter n (maximum index in the sequence): ");
-        scanf("%d", &number_of_elements);
+        scanf("%d", &n);
     }
 
-    return number_of_elements;
+    return n;
 }
 
 /* Main function gets and checks command line arguments and users input, 
@@ -148,14 +147,14 @@ creates and prints fibonnaci sequence, and frees the memory that was allocated w
 Return NO_ERRORS when run completed successfuly. */
 int main(int argc, char* argv[]) {
     /* Get and check command line arguments and users input */
-    int number_of_elements = NULL;
-    char* file_name = check_arguments(argc, argv);
-    number_of_elements = check_elements_number(number_of_elements);
+    int n = NULL;
+    char* file_name = get_file_name(argc, argv);
+    n = get_n_input(n);
 
     /* Create and print fibonnaci sequence */
-    Node* first_node = create_fibonacci(number_of_elements);
-    write_fibonacci_file(first_node, number_of_elements, file_name);
-    print_fibonacci(first_node, number_of_elements);
+    Node* first_node = create_fibonacci(n);
+    write_fibonacci_file(first_node, n, file_name);
+    print_fibonacci(first_node, n);
     
     /* Free the memory that was allocated with malloc */
     free_memory(&first_node);
--- Mmn23/fibon.h
+++ Mmn23/fibon.h
@@ -18,38 +18,38 @@ typedef struct Node {
 It loops through the nodes in the sequence and uses free function to free the memory. Does not return anything. */
 void free_memory(Node** first_node_pointer);
 
-/* Write_fibonnaci_file function gets a Node pointer to the first node, the number of elements in the sequence, and a character pointer to the file name.
+/* Write_fibonnaci_file function gets a Node pointer to the first node, the maximum index in the sequence, and a character pointer to the file name.
 It creates a file, prints an error if one was encountered, and prints a title.
 Then it loops through the sequence and prints the sequence in a descending order.
 Finally, it closes the file. Does not return anything. */
-void write_fibonacci_file(Node* first_node, int number_of_elements, char* file_name);
+void write_fibonacci_file(Node* first_node, int n, char* file_name);
 
-/* Print_fibonnaci function gets a Node pointer to the first node and the number of elements in the sequence.
+/* Print_fibonnaci function gets a Node pointer to the first node and the the maximum index in the sequence.
 It prints the sequence in a descending order. Does not return anything. */
-void print_fibonacci(Node* first_node, int number_of_elements);
+void print_fibonacci(Node* first_node, int n);
 
 /* Create_node function gets an unsigned long long number, which will be the node data.
 It creates a new node- allocates memory to it, and sets its number to the number it recieved.
 Returns the new node it created. */
 Node* create_node(unsigned long long number);
 
-/* Create_fibonacci function gets the number of elements the user entered and creates the fibonnaci sequence.
+/* Create_fibonacci function gets n- maximum index in the sequence the user entered and creates the fibonnaci sequence.
 * The function uses create_node function in order to create each node in the sequence.
-First, it creates the first node with number 0, checks if the number of elements is zero, and if so,
+First, it creates the first node with number 0, checks if n is zero, and if so,
 it returns the first node which points to itself. Then, it creates the second node with the number 1.
-After that, it loops as long as the index of the sequence is smaller than the number of elements,
+After that, it loops as long as the index of the sequence is smaller than the n,
 and creates a circular ascending order fibonnaci sequence. Then, it reverses the order of the sequence to a descending order.
 Finally, it returns a pointer to the first node. */
-Node* create_fibonacci(int number_of_elements);
+Node* create_fibonacci(int n);
 
-/* Check_arguments function gets the number of arguments- int argc,
+/* Get_file_name function gets the number of arguments- int argc,
 and a pointer array which points to each argument passed to the program in the command line- char* argv[].
 The function checks that the number of arguments that were passed in the command line is valid,
 if it is not, it print an error message and exit the program.
 If no errors were encounterd, returns the file name which is a pointer to characters */
-char* check_arguments(int argc, char* argv[]);
+char* get_file_name(int argc, char* argv[]);
 
-/* Check_elements_number gets the number of elements the user entered- int number_of_elements.
+/* Get_n_input function gets the initial n- int n.
 The function checks that the number is positive, if it is not, it prints an error message ands asks again for an input.
-Returns a valid number of elements- int number_of_elements */
-int check_elements_number(int number_of_elements);
+Returns a valid number of n (maximum index in fibonacci sequence)- int n */
+int get_n_input(int n);
