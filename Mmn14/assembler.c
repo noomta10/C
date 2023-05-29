3daac365f61321fa607bb31087cc0eba8b1111b3
noomt <noomta10@gmail.com>
2023-05-29 16:43:02 +0300

    ooo

--- Mmn14/assembler.c
+++ Mmn14/assembler.c
@@ -1,26 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "assembler.h" 
 
-static boolean process_file(char* file_name);
+static void process_file(char* file_name);
 
 int main(int argc, char* argv[]) {
 	int i;
-	/* To break line if needed */
-	boolean succeeded = TRUE;
+	
 	/* Process each file by arguments */
 	for (i = 1; i < argc; ++i) {
-		/* if last process failed and there's another file, break line: */
-		if (!succeeded) puts("");
-		/* foreach argument (file name), send it for full processing. */
-		succeeded = process_file(argv[i]);
-		/* Line break if failed */
+		process_file(argv[i]);
 	}
 	return 0;
 }
 
 
-
-static boolean process_file(char* file_name) {
+static void process_file(char* file_name) {
 	FILE* file_pointer = NULL;
 
 	/* Concatenate '.as' postfix to file name */
@@ -32,9 +26,8 @@ static boolean process_file(char* file_name) {
 	if (file_pointer == NULL) {
 		printf("Error: The file %s couldn't be opened\n", full_file_name);
 		free(full_file_name);
-		return FALSE;
+		return;
 	}
 
 	free(full_file_name);
-	return TRUE;
 }
