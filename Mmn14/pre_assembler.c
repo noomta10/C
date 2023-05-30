6876db91e58f9e820acb1bad5b85c7adf2e4ea58
noomt <noomta10@gmail.com>
2023-05-30 10:33:29 +0300

    b

--- Mmn14/pre_assembler.c
+++ Mmn14/pre_assembler.c
@@ -2,21 +2,32 @@
 #include "pre_assembler.h"
 #include "assembler.h"
 #include "utils.h"
+#include <string.h>
 
 void pre_assembler(FILE* source_file) {
 	char line[MAX_LINE_LENGTH];
+	mcros_table_entry first_mcro = {NULL, NULL, NULL};
 
 	/* Read file line by line until the end */
 	while (fgets(line, sizeof(line), source_file) != NULL) {
-		printf("%s\n", get_next_element(line));  // Do something with the line (printing in this case)
+		char* first_word = get_next_element(line);
+		if (strcmp(first_word, "mcro")) {
+
+		}
 	}
 
+	/* Close the file */
 	fclose(source_file);  // Close the file
 }
 
 
 
-static void add_mcro_to_table(char* mcro_name, char* mcro_value, mcros_table_entry* mcros_table_entry)
+static void add_mcro_to_table(char* mcro_name, char* mcro_value, mcros_table_entry* first_mcro)
 {
 	return;
+}
+
+
+static boolean mcro_in_table(mcros_table_entry* mcros_table_entry) {
+
 }
