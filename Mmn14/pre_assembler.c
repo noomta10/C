34c50c8e1c860dae76694b9ba6571ab73c09a3eb
noam <noomta10@gmail.com>
2023-06-02 18:52:51 +0300

    works in windows not in ubuntu

--- Mmn14/pre_assembler.c
+++ Mmn14/pre_assembler.c
@@ -8,8 +8,67 @@
 #include "utils.h"
 
 
+static void free_table_memory(mcros_table_entry** first_mcro_entry) {
+	mcros_table_entry* current_mcro_entry = *first_mcro_entry;
+	mcros_table_entry* next_mcro_entry;
+	
+	/* Free mcros table */
+	while (current_mcro_entry->next != NULL) {
+		next_mcro_entry = current_mcro_entry->next;
+		free(current_mcro_entry->name);
+		free(current_mcro_entry->value);
+		free(current_mcro_entry);
+		current_mcro_entry = next_mcro_entry;
+	} 
+
+	/* Free last mcro entry */
+	free(current_mcro_entry->name);
+	free(current_mcro_entry->value);
+	free(current_mcro_entry);
+
+}
+
+
+static void rename_file(char* file_name) {
+	char* new_file_name = add_file_postfix(file_name, ".am");
+
+	/* If file with '.am' postfix exists, remove it */
+	FILE* new_file = fopen(new_file_name, "r");
+	if (new_file != NULL) {
+		fclose(new_file);
+		remove(new_file_name);
+	}
+
+	(void)rename("template_file.txt", new_file_name);
+}
+
+
+static boolean handle_existing_mcro(mcros_table_entry** first_mcro_entry, char* first_word, FILE* template_file) {
+	mcros_table_entry* current_mcro_entry = *first_mcro_entry;
+
+	/* If the mcros table is empty, return FALSE */
+	if (first_mcro_entry == NULL) {
+		return FALSE;
+	}
+
+	/* Loops through the table to check if the first word is a defined macro in the table */
+	while (current_mcro_entry) {
+		/* If the first word is a defined macro in the table, write its value to the template file, and return TRUE */
+		if (strcmp(first_word, current_mcro_entry->name) == 0) {
+			fprintf(template_file, "%s", current_mcro_entry->value);
+			return TRUE;
+		}
+
+		current_mcro_entry = current_mcro_entry->next;
+	}
+
+	return FALSE;
+}
+
+
 static void add_mcro_to_table(char* mcro_name, char* mcro_value, mcros_table_entry** first_mcro_entry) {
 	mcros_table_entry* new_mcro_entry = malloc_with_check(sizeof(mcros_table_entry));
+	mcros_table_entry* current_mcro_entry;
 	int total_length;
 
 	/* If table is empty, add first mcro */
@@ -23,7 +82,7 @@ static void add_mcro_to_table(char* mcro_name, char* mcro_value, mcros_table_ent
 		return;
 	}
 
-	mcros_table_entry* current_mcro_entry = *first_mcro_entry;
+	current_mcro_entry = *first_mcro_entry;
 
 	/* If mcro already exists in the table, append value to mcro */
 	while (current_mcro_entry->next != NULL) {
@@ -60,59 +119,25 @@ static void add_mcro_to_table(char* mcro_name, char* mcro_value, mcros_table_ent
 }
 
 
-static boolean handle_existing_mcro(mcros_table_entry** first_mcro_entry, char* first_word, FILE* template_file) {
-	mcros_table_entry* current_mcro_entry = *first_mcro_entry;
-
-	/* If the mcros table is empty, return FALSE */
-	if (first_mcro_entry == NULL) {
-		return FALSE;
-	}
-	
-	/* Loops through the table to check if the first word is a defined macro in the table */
-	while (current_mcro_entry) {
-		/* If the first word is a defined macro in the table, write its value to the template file, and return TRUE */
-		if (strcmp(first_word, current_mcro_entry->name) == 0) {
-			fprintf(template_file, "%s", current_mcro_entry->value);
-			return TRUE;
-		}
-
-		current_mcro_entry = current_mcro_entry->next;
-	}
-
-	return FALSE;
-}
-
-
-static void rename_file(char* file_name) {
-	char* new_file_name = add_file_postfix(file_name, ".am");
-
-	/* If file with '.am' postfix exists, remove it */
-	FILE* new_file = fopen(new_file_name, "r");
-	if (new_file != NULL) {
-		fclose(new_file);
-		remove(new_file_name);
-	}
-
-	(void)rename("template_file.txt", new_file_name);
-}
-
-
 void pre_assembler(FILE* source_file, char* file_name) {
 	char line[MAX_LINE_LENGTH];
 	mcros_table_entry* first_mcro_entry = NULL;
 	char* mcro_name = NULL;
+	char* saved_line = NULL;
+	char* saved_mcro_name = NULL;
+	char* first_word;
 
 	/* Create an empty file, close it, an reopen it in append mode */
 	FILE* template_file = fopen("template_file.txt", "w");
 	if (template_file == NULL) {
-		printf("Error: The file %s could not be opened\n", template_file);
+		printf("Error: The file 'template_file.txt' could not be opened\n");
 		return;
 	}
 
 	fclose(template_file);
 	template_file = fopen("template_file.txt", "a");
 	if (template_file == NULL) {
-		printf("Error: The file %s could not be opened\n", template_file);
+		printf("Error: The file 'template_file.txt' could not be opened\n");
 		return;
 	}
 
@@ -120,9 +145,9 @@ void pre_assembler(FILE* source_file, char* file_name) {
 	while (fgets(line, sizeof(line), source_file) != NULL) {
 		
 		/* Allocate memory for the line, and copy it */
-		char* saved_line = malloc_with_check(sizeof(line));
+		saved_line = malloc_with_check(sizeof(line));
 		strcpy(saved_line, line);
-		char* first_word = strtok(line, " \t\n");
+		first_word = strtok(line, " \t\n");
 
 		/* If it is an empty line, print it to the template file continue to next line */
 		if (first_word == NULL) {
@@ -140,13 +165,13 @@ void pre_assembler(FILE* source_file, char* file_name) {
 			mcro_name = strtok(NULL, " \t\n");
 
 			/* Allocate memory for the mcro name, and copy it */
-			char* saved_mcro_name = malloc_with_check(strlen(mcro_name) + 1);
+			saved_mcro_name = malloc_with_check(strlen(mcro_name) + 1);
 			strcpy(saved_mcro_name, mcro_name); 
 
 			/* Mcros definition, adds its value to the table */
-			while(TRUE) {
+			while (TRUE) {
 				fgets(line, sizeof(line), source_file);
-				char* saved_line = malloc_with_check(sizeof(line));
+				saved_line = malloc_with_check(sizeof(line));
 				strcpy(saved_line, line);
 				first_word = strtok(line, " \t\n");
 
@@ -165,10 +190,14 @@ void pre_assembler(FILE* source_file, char* file_name) {
 		fprintf(template_file, "%s", saved_line);
 	}
 
-	/* Close the files */
-	fclose(source_file);
+	/* Close the file */
 	fclose(template_file);
 
 	/* Rename template file to '.am' file */
 	rename_file(file_name);
+
+	/* free allocated memory */
+	free(saved_line);
+	free(saved_mcro_name);
+	free_table_memory(&first_mcro_entry);
 }
