f8eb6809dd3d876823d2f4a711d08ed3d65480ff
noomt <noomta10@gmail.com>
2023-06-12 08:31:37 +0300

    try base64

--- try_project/try.c
+++ try_project/try.c
@@ -1,19 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-char* add_file_postfix(char* file_name, char* postfix) {
-	char* full_file_name = (char*)malloc(strlen(file_name) + strlen(postfix) + 1);
-	strcpy(full_file_name, file_name);
-	strcat(full_file_name, postfix);
-	return full_file_name;
+// Base64 character mapping table
+static const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+// Function to encode a 6-bit number to base64
+char base64_encode_bits(uint8_t bits) {
+    return base64_chars[bits];
 }
 
 int main() {
-	FILE* my_file = fopen("my_file.txt", "w");
-	fclose(my_file);
-	if (rename("my_file.txt", "new_name.txt") != 0)
-		printf("Error renaming file\n");
+    unsigned int number = 0b101010101010; // Example unsigned integer
 
+    // Extract the two 6-bit groups
+    uint8_t group1 = (number >> 6) & 0x3F;
+    uint8_t group2 = number & 0x3F;
 
+    // Encode each group using base64
+    char encoded_char1 = base64_encode_bits(group1);
+    char encoded_char2 = base64_encode_bits(group2);
+
+    // Print the base64 encoded characters
+    printf("Base64 Encoded Characters: %c%c\n", encoded_char1, encoded_char2);
+
+    return 0;
 }
